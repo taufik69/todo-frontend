60b57d9a7ba33acdee2f768617325783aa6fa713
taufik69 <taufik35-2915@diu.edu.bd>
2023-08-01 16:51:51 +0600

    todo first setup

---
+++ src/Component/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="flex min-h-screen">
+        <div className="w-3/5 bg-black"></div>
+        <div className="w-2/5 bg-red-500"></div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
